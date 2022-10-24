0aeccc7b379bcfc0cbc077ba96f67453ada88c0e
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-24 12:45:30 -0300

    inicio do projeto portifolio

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+const botaoMenu = document.querySelector('.cabecalho__menu')
+const menu = document.querySelector('.menu-lateral')
+
+botaoMenu.addEventListener('click', () => {
+    alert("click")
+    // menu.classList.toggle('menu_lateral__ativo')
+})
