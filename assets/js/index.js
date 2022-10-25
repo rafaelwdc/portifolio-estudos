f4a6f82f9f45274f027d9e8c436845c592ad8ae3
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-25 14:43:59 -0300

    update main

--- assets/js/index.js
+++
@@ -1,7 +0,0 @@
-const botaoMenu = document.querySelector('.cabecalho__menu')
-const menu = document.querySelector('.menu-lateral')
-
-botaoMenu.addEventListener('click', () => {
-    alert("click")
-    // menu.classList.toggle('menu_lateral__ativo')
-})
