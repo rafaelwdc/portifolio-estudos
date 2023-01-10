be04050cc9bcd9214becc09916e8ab8d38e0e5bb
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-09 22:51:16 -0300

    att index.js-Carrinho

--- portifolio-react/alura-books/src/componentes/Carrinho/index.js
+++ portifolio-react/alura-books/src/componentes/Carrinho/index.js
@@ -5,7 +5,6 @@ export default function Carrinho() {
   return (
     <div className='display'>
       <div className='carrinho'></div>
-      <h2 className='tituloIcone'>Carrinho</h2>
     </div>
   )
 }
