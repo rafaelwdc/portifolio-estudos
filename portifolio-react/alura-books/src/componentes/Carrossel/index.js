79ed61684bb637cd40fbdc830a5f6360d88bd8af
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-04-06 19:46:44 -0300

    Atualizando Carrossel/index.js

--- portifolio-react/alura-books/src/componentes/Carrossel/index.js
+++ portifolio-react/alura-books/src/componentes/Carrossel/index.js
@@ -5,15 +5,37 @@ import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import './Carrossel.css'
+import livros from '../../assets/livros.json'
+import styles from './Carrossel.css'
 
-export default function Carrossel({ settings, children }) {
+export default function Carrossel() {
+    const settings = {
+        spaceBetween: 10,
+        slidesPerView: 3,
+        navigation: true,
+        pagination: {
+            clickable: true,
+        }
+    }
     return (
         <>
             <Swiper
                 modules={[Navigation, Pagination, A11y]}
                 {...settings}
             >
-                {children}
+                {
+                    livros.map(livro => {
+                        return (
+                            <ul className={styles.carrosselSlide}>
+                                <SwiperSlide>
+                                    <li key={livro.id}>
+                                        <img src={livro.path} alt={livro.subtitulo} />
+                                    </li>
+                                </SwiperSlide>
+                            </ul>
+                        )
+                    })
+                }
             </Swiper>
         </>
     )
